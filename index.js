cef8080fb5dc44269a888f1b29d95b9ec3dca3e6
Algernon Soriano <alger.soriano@gmail.com>
2022-08-31 21:10:17 +1000

    looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = []
+var thankMsg = []
+
+function writeCards( receiver , greeting ) {
+    // console.log(receiver.length)
+    for (let i=0; i<receiver.length; i++) {
+     thankMsg.push(`Thank you, ${receiver[i]}, for the wonderful ${greeting} gift!`)
+     
+    } 
+
+     return thankMsg
+}
+writeCards(names, 'surprise')
+
+function countDown(number) {
+    for (let i = number; i>-1; i--)
+        console.log(i)
+
+}
+countDown(10)
